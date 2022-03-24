a744c48f256a0325b11faa899788615bc7adac86
Finley Edwards <EDW17006550@btc.ac.uk>
2022-03-24 14:45:32 +0000

    minor script changes

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -24,7 +24,6 @@ if (darkStatus === true){
     document.getElementById("main-nav").classList.toggle("bg-dark");
     document.getElementById("nav-dropdown").classList.toggle("dropdown-current-page");
     document.getElementById("nav-dropdown").classList.toggle("nav-dropdown-dark");
-    //document.getElementById("title").classList.toggle("title");
     document.getElementById("title").classList.toggle("title-dark");
     document.getElementById("main-content").classList.toggle("content-dark");
     document.getElementById("footer-spacing").classList.toggle("footer-spacing-dark");
@@ -32,14 +31,14 @@ if (darkStatus === true){
     document.getElementById("facebook-logo").src="images/facebook_inverted.png";
     document.getElementById("twitter-logo").src="images/twitter_inverted.png";
 
-    if (window.location.href === "/menus" || window.location.href === "/breakfasts" || window.location.href === "/burgers_chips_jackets" || window.location.href === "/paninis_wraps_roasts" || window.location.href === "/cold_fillings_salads"){
+    try{
         document.getElementById("current-dropdown").classList.toggle("dropdown-current-page-dark");
-    }
 
-    var cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-        cards[i].classList.toggle("card-dark");
-    }
+        var cards = document.getElementsByClassName("card");
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].classList.toggle("card-dark");
+        }
+    } catch{}
 }
 
 function toggleDark(){
@@ -48,22 +47,21 @@ function toggleDark(){
     document.getElementById("main-nav").classList.toggle("bg-dark");
     document.getElementById("nav-dropdown").classList.toggle("dropdown-current-page");
     document.getElementById("nav-dropdown").classList.toggle("nav-dropdown-dark");
-    //document.getElementById("title").classList.toggle("title");
     document.getElementById("title").classList.toggle("title-dark");
     document.getElementById("main-content").classList.toggle("content-dark");
     document.getElementById("footer-spacing").classList.toggle("footer-spacing-dark");
     document.getElementById("footer").classList.toggle("footer-dark");
 
-    if (window.location.href === "/menus" || window.location.href === "/breakfasts" || window.location.href === "/burgers_chips_jackets" || window.location.href === "/paninis_wraps_roasts" || window.location.href === "/cold_fillings_salads"){
+    try{
         document.getElementById("current-dropdown").classList.toggle("dropdown-current-page-dark");
-    }
 
-    var cards = document.getElementsByClassName("card");
-    for (var i = 0; i < cards.length; i++) {
-        cards[i].classList.toggle("card-dark");
-    }
-      
-
+        var cards = document.getElementsByClassName("card");
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].classList.toggle("card-dark");
+        }
+    } catch{}
+    
+        
 
     if (darkStatus === true){
         darkStatus = false;
